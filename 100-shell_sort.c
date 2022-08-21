b16a46c922c853aa9c750f1d021f06a3cef6c780
Talabi, Oluwasefunmi Joy <oluwaseyifunmmyjoyce1@gmail.com>
2022-08-21 01:29:14 +0100

    New update for sorting algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,46 @@
 #include "sort.h"
+#include <stdio.h>
+/**
+ * _swap - swaped 2 values.
+ * @array: the array for swap him values.
+ * @i: First index
+ * @j: Second index
+ * Return: Nothing
+ */
+void _swap(int *array, int i, int j)
+{
+	int tmp;
+
+	if (array[i] != array[j])
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+}
 
 /**
- * shell_sort - sort algorithm
- * @array: array to sort
- * @size: size of the array
+ * shell_sort - sort the list and print the changes
+ * @array: The array to sort.
+ * @size: Size of the array
+ * Return: Nothing
  */
 void shell_sort(int *array, size_t size)
 {
-int inner, outer;
-int valueToInsert;
-int interval = 1;
-int i = 0;
+	size_t h = 0,  i, j;
 
-while(interval <= (int) size/3)
-	interval = interval*3 +1;
+	if (size < 2)
+		return;
 
+	while (h <= size / 3)
+		h = h * 3 + 1;
 
-while(interval > 0) {
-
-	for(outer = interval; outer < (int) size; outer++) {
-		valueToInsert = array[outer];
-		inner = outer;
-
-		while(inner > interval -1 && array[inner - interval] >= valueToInsert) {
-		array[inner] = array[inner - interval];
-		inner -=interval;
-		}
-
-		array[inner] = valueToInsert;
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
+				_swap(array, j, j - h);
+		h /= 3;
+		print_array(array, size);
 	}
-
-interval = (interval -1) /3;
-print_array(array, size);
-i++;
-}
 }
